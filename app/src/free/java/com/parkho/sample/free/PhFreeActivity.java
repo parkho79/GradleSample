78f92dc2220e9ce13d9d3fc160cc529ebf59bc6c
parkho79 <parkho79@gmail.com>
2020-04-22 10:52:23 +0900

    GradleSample

---
+++ app/src/free/java/com/parkho/sample/free/PhFreeActivity.java
@@ -0,0 +1,15 @@
+package com.parkho.sample.free;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parkho.sample.R;
+
+public class PhFreeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/paid/java/com/parkho/sample/paid/PhPaidActivity.java
@@ -0,0 +1,15 @@
+package com.parkho.sample.paid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.parkho.sample.R;
+
+public class PhPaidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
